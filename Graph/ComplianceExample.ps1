6223ca790c42a5aec8e8c0fd51d351954d2f8bcc
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2020-05-08 21:33:14 +0200

    Update ComplianceExample.ps1

--- Graph/ComplianceExample.ps1
+++ Graph/ComplianceExample.ps1
@@ -1,19 +1,38 @@
+#Unformatted
+@odata.type: "#microsoft.graph.windows10CompliancePolicy"
+bitLockerEnabled: true
+deviceThreatProtectionEnabled: false
+deviceThreatProtectionRequiredSecurityLevel: "unavailable"
+displayName: "Windows10-Compliance"
+id: "00000000-0000-0000-0000-000000000000"
+passwordRequiredType: "deviceDefault"
+roleScopeTagIds: ["0"]
+0: "0"
+scheduledActionsForRule: [{ruleName: "PasswordRequired", scheduledActionConfigurations: [,…]}]
+0: {ruleName: "PasswordRequired", scheduledActionConfigurations: [,…]}
+ruleName: "PasswordRequired"
+scheduledActionConfigurations: [,…]
+0: {actionType: "block", gracePeriodHours: 0, notificationTemplateId: "", notificationMessageCCList: []}
+actionType: "block"
+gracePeriodHours: 0
+notificationMessageCCList: []
+notificationTemplateId: ""
+
+<# POWERSHELL #>
 Install-Module -Name Microsoft.Graph.Intune
 Connect-MSGraph
 
-
- #Windows 10 Compliance
- $Win10Compliance = New-IntuneDeviceCompliancePolicy `
-    -windows10CompliancePolicy `
-    -displayName "Win10-Compliance" `
-    -osMinimumVersion 10.0.16299 `
-    -scheduledActionsForRule `
-    (New-DeviceComplianceScheduledActionForRuleObject `
-        -ruleName PasswordRequired `
-        -scheduledActionConfigurations `
-        (New-DeviceComplianceActionItemObject `
-            -gracePeriodHours 0 `
-            -actionType block `
-            -notificationTemplateId "" `
-        ) `
-    )
+$Win10Compliance = New-IntuneDeviceCompliancePolicy `
+ -windows10CompliancePolicy `
+ -displayName "Win10-Compliance" `
+ -osMinimumVersion 10.0.16299 `
+ -scheduledActionsForRule `
+  (New-DeviceComplianceScheduledActionForRuleObject `
+  -ruleName PasswordRequired `
+  -scheduledActionConfigurations `
+    (New-DeviceComplianceActionItemObject `
+    -gracePeriodHours 0 `
+    -actionType block `
+    -notificationTemplateId "" `
+    )`
+  )
