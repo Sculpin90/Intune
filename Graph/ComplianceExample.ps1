fab3eed0ad7c895fee434a20d00a425ec37b151f
0fflineDocs <simon.ludvig.hakansson@outlook.com>
2020-05-08 21:48:07 +0200

    Update ComplianceExample.ps1

--- Graph/ComplianceExample.ps1
+++ Graph/ComplianceExample.ps1
@@ -1,10 +1,11 @@
 #Unformatted
+{id: "00000000-0000-0000-0000-000000000000", displayName: "Windows10-Compliance",…}
 @odata.type: "#microsoft.graph.windows10CompliancePolicy"
-bitLockerEnabled: true
 deviceThreatProtectionEnabled: false
 deviceThreatProtectionRequiredSecurityLevel: "unavailable"
 displayName: "Windows10-Compliance"
 id: "00000000-0000-0000-0000-000000000000"
+osMinimumVersion: "10.0.18363.778"
 passwordRequiredType: "deviceDefault"
 roleScopeTagIds: ["0"]
 0: "0"
@@ -22,17 +23,18 @@ notificationTemplateId: ""
 Install-Module -Name Microsoft.Graph.Intune
 Connect-MSGraph
 
-$Win10Compliance = New-IntuneDeviceCompliancePolicy `
- -windows10CompliancePolicy `
- -displayName "Win10-Compliance" `
- -osMinimumVersion 10.0.16299 `
- -scheduledActionsForRule `
-  (New-DeviceComplianceScheduledActionForRuleObject `
-  -ruleName PasswordRequired `
-  -scheduledActionConfigurations `
-    (New-DeviceComplianceActionItemObject `
-    -gracePeriodHours 0 `
-    -actionType block `
-    -notificationTemplateId "" `
-    )`
-  )
+#Windows 10 Compliance
+ $Windows10Compliance = New-IntuneDeviceCompliancePolicy `
+    -windows10CompliancePolicy `
+    -displayName "Windows10-Compliance" `
+    -osMinimumVersion 10.0.18363.778 `
+    -scheduledActionsForRule `
+    (New-DeviceComplianceScheduledActionForRuleObject `
+        -ruleName PasswordRequired `
+        -scheduledActionConfigurations `
+        (New-DeviceComplianceActionItemObject `
+            -gracePeriodHours 0 `
+            -actionType block `
+            -notificationTemplateId "" `
+        ) `
+    )
